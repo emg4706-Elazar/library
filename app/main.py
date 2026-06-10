d6db27d753d326b888e86b4c0546d3f9d7a445f1
אלעזר גרשי <emg4706@gmail.com>
2026-06-11 00:00:59 +0300

    checked first connection to api

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+
+app = FastAPI()
+
+@app.get("/library")
+def sanity():
+    return "Hello world"
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
